b92ddd80c741a51b517e63a5a529f2e34b4ea08e
Renan Ribeiro <62890581+Horumaru@users.noreply.github.com>
2020-11-20 17:44:55 -0300

    Create LogDeslog.js

---
+++ LogDeslog.js
@@ -0,0 +1,25 @@
+/*
+Reconhecer o botão
+Adicionar cronometro /  reconhecer relógio
+Clicar no botão
+*/
+
+/*
+reconhecer o botão
+*/
+document.getElementsByClassName('PSPUSHBUTTON')[5].id
+for (var i=0; i < logss.length; i++){
+		console.log(i);
+    if(logss[i].id == 'IN_BTN'){
+        console.log (logss[i].id)
+    }
+}
+
+/*
+Adicionar cronometro /  reconhecer relógio
+*/
+
+/*
+clicar no botão
+*/
+document.getElementsByClassName('PSPUSHBUTTON')[5].click();
