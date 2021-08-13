cb4e99c14de327744d964f8e5fea27003af80d9c
Renan Ribeiro <62890581+Horumaru@users.noreply.github.com>
2021-08-13 14:33:29 -0300

    Update element_ativo.js

--- Codigos JS/element_ativo.js
+++ Codigos JS/element_ativo.js
@@ -1,22 +1,24 @@
 document.addEventListener('submit', function(){
-    console.log('Form submit')
+	console.log('Form submit')
     function elementAtivo() {
-       var interval = setInterval(() => {
-         var cont = 0
-          var element = document.getElementsByClassName('et-pb-contact-message')[0].innerText == "Obrigado por entrar em contato conosco"
-          if (element) {
-              console.log(element);
-              clearInterval(interval)
-              //window.location.reload();
-          }
-         else{
-            cont += 1
-           console.log(cont)
-           if (cont > 10){
-             console.log('break')
-             break();
-           }
-         }
-        }, 1000);
+    	var cont = 0
+       	var interval = setInterval(() => { 
+        	var element = document.getElementsByClassName('et-pb-contact-message')[0].innerText
+            var texto = "Obrigado por entrar em contato conosco"
+          	if (element== texto) {
+            	console.log(element);
+              	clearInterval(interval)
+              	//window.location.reload();
+          	}
+         	else{
+           		cont ++
+           		console.log(cont)
+			   if (cont > 10){
+				 console.log('break')
+				 clearInterval(interval)
+			   }
+         	}
+		}, 1000);
     }
+    elementAtivo()
 });
